1b0653bc28b07b7a428da4fd65b2a533a451e59e
rox <qt02537@gmail.com>
2020-05-06 20:18:39 +0000

    Ch12 activating route with code - start

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +10,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) {
+  }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'May 6, 2020',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
